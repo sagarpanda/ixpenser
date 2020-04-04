3850480acb342735495720aab2840a61f68f6584
sagarpanda <er.sagarpanda@gmail.com>
2020-04-04 21:46:25 +0000

    Deploying to gh-pages from  @ 0c8172310c55e2f15a89ad1af5fd801e8f156087 🚀

---
+++ precache-manifest.cfe7aff070dbff2c1a32c492039cc1a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140cf93afdc0634f64e6cafd64029bce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18b1cf829ccb5e3956ad",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ae67f4ea41ca467849e3",
+    "url": "/static/js/2.e53d6608.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e53d6608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b1cf829ccb5e3956ad",
+    "url": "/static/js/main.948d14cc.chunk.js"
+  },
+  {
+    "revision": "aed91179c703a84c62d7",
+    "url": "/static/js/runtime-main.20a8494a.js"
+  }
+]);
